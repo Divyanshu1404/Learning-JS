967c8424a9dcd57f56b0c74e40bbac6c8e8c6873
DIVYANSHU MISHRA <145105300+Divyanshu1404@users.noreply.github.com>
2025-10-13 10:32:50 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+console.log("namaste duniya")
+console.log(3+5);
+
+
+
+var age =25;
+
+if(true){
+    console.log(age);
+}
+
+
+const x = 20;
+console.log(x);     
+
+const y = 30;
+console.log(y);     
---
+++ variable.js
@@ -0,0 +1,22 @@
+
+var age =25;
+
+if(true){
+    console.log(age);
+}
+
+
+// Re assignment using 'let'
+
+let z = 10;
+console.log(z);    // Output : 10
+
+z = "Mayank";
+console.log(z);    // Output : Mayank
+
+
+const x = 20;
+console.log(x);     
+
+const y = 30;
+console.log(y);     
