84f141436bae26b4f2a0252133340302837cfe7f
DIVYANSHU MISHRA <mishraaman4512@gmail.com>
2026-02-05 14:41:06 +0530

    OI

--- index.js
+++
@@ -1,3 +0,0 @@
-console.log("my name is Aman")
-console.log("I am learning Node.js")
-
--- script.js
+++ script.js
@@ -1,10 +1,37 @@
-var a=5;
-var a=6;
+// var a=5;
+// var a=6;
 
-let c=7;
-
-
-const dulha="LAb";
-const dulhan="laby";
+// let c=7;
 
 
+// const dulha="LAb";
+// const dulhan="laby";
+
+
+// Resignment
+// var a=32;
+// a=65;
+
+// let b=23;
+//  b=70
+//  console.log(a+b);
+
+//Temporal DEAD zone   
+// console.log(a);
+// let 0;
+
+// Hoisting
+
+// var a=undefined;
+// a=12;
+
+//  console.log(a);
+//  let a=5;
+
+
+let a=50;
+{
+    let a=67;
+    console.log("inside: ",a);
+}
+console.log("outside: ",a);
